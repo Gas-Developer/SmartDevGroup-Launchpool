659e7d2fc9bbd9f5863a0160568fb4e0e23e131d
Gas-Developer <gascriptocoin@gmail.com>
2023-07-23 10:56:08 +0200

    Claim function on smart-contract

--- contracts/launchpool.sol
+++ contracts/launchpool.sol
@@ -9,6 +9,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Launchpool is Ownable, ReentrancyGuard {
+
+	using SafeMath for uint256;
+
 	ERC20 public token;
 
 	// EVENTS
@@ -37,6 +40,11 @@ contract Launchpool is Ownable, ReentrancyGuard {
 	);
 	event newEndSetted(
 		uint256 end
+	);
+	event tokenClaimed(
+		address indexed sender,
+		uint256 amount,
+		uint256 remainingTokenToDistribute
 	);
 
 	// DATA STRUCTURES & VARIABLES
@@ -126,7 +134,7 @@ contract Launchpool is Ownable, ReentrancyGuard {
 		require(totalTokenToDistribute > 0, "No tokens to distribute");		// Controllo che ci siano ancora token da distribuire
 
 		uint256 orderID = orders.length; 									// Assegno l'ID dell'ordine
-		console.log("orderID: ", orderID);
+		//console.log("orderID: ", orderID);
 
 		// Creo l'order
 		Order memory senderOrder;											// Creo un nuovo ordine
@@ -136,21 +144,21 @@ contract Launchpool is Ownable, ReentrancyGuard {
 		senderOrder.isClaimed = false;										// Assegno il valore false al claim
 
 		// Inserisco l'order nella lista degli order
-		orders.push(senderOrder); // Aggiungo l'ordine all'array degli ordini
+		orders.push(senderOrder); 											// Aggiungo l'ordine all'array degli ordini
 
 		// Assegno l'order all'address che ha effettuato lo stake
-		orderIDs[msg.sender].push(orderID); // Associo l'ID dell'ordine all'address che ha effettuato lo stake
+		orderIDs[msg.sender].push(orderID); 								// Associo l'ID dell'ordine all'address che ha effettuato lo stake
 
 		// Aggiorno il totale dei power
-		TotalPower = TotalPower + senderOrder.power; // Aggiungo il power al totale dei power
+		TotalPower = TotalPower + senderOrder.power;						// Aggiungo il power al totale dei power
 
-		console.log("senderOrder.stakedAmount: ", senderOrder.stakedAmount);
-		console.log("endLP: ", endLP);
-		console.log("senderOrder.orderTime: ", senderOrder.orderTime);
-		console.log("(endLP - senderOrder.orderTime) = ", (endLP - senderOrder.orderTime));
-		console.log(senderOrder.stakedAmount, " *  ", (endLP - senderOrder.orderTime), " = ");
+		//console.log("senderOrder.stakedAmount: ", senderOrder.stakedAmount);
+		//console.log("endLP: ", endLP);
+		//console.log("senderOrder.orderTime: ", senderOrder.orderTime);
+		console.log("[", orderID, "]: (endLP - senderOrder.orderTime) = ", (endLP - senderOrder.orderTime));
+		console.log(senderOrder.stakedAmount, " * ", (endLP - senderOrder.orderTime), " = ");
 		console.log("senderOrder.power: ", senderOrder.power);
-		console.log("New TotalPower: ", TotalPower);
+		//console.log("New TotalPower: ", TotalPower);
 
 		emit newStakeOrder(
 			msg.sender,
@@ -159,6 +167,58 @@ contract Launchpool is Ownable, ReentrancyGuard {
 			TotalPower
 		);
 	}
+
+
+   function claim() public launchpoolEnded nonReentrant {
+		require(orderIDs[msg.sender].length > 0, "No orders for this address");								// Controllo che l'address abbia effettuato almeno uno stake
+
+		uint256 totalPowerToClaim = 0; // Inizializzo il totale del power dell'utente
+
+		for (uint256 i = 0; i < orderIDs[msg.sender].length; i++) {
+			uint256 currentOrderID = orderIDs[msg.sender][i];												// Recupero l'ID dell'ordine che sto processando dal mapping orderIDs
+			Order memory currentOrder = orders[currentOrderID];												// Recupero l'ordine che sto processando dall'array orders
+
+			if (currentOrder.isClaimed) {
+				continue;																					// Se l'ordine è già stato claimato passo al prossimo
+			}
+
+			// Se l'ordine non è stato già riscosso (Claimed), per prima cosa lo imposto a riscosso isClaimed = true (check, effect, interaction)
+			// naturalmente setto isClaimed sull'array globale degli orders e non su currentOrder che è una copia locale dell'ordine
+			orders[currentOrderID].isClaimed = true;
+
+			totalPowerToClaim = totalPowerToClaim.add(currentOrder.power);									// Aggiungo il power dell'ordine corrente al totale del power dell'utente
+		}
+
+		//console.log("userTotalPowerToClaim: ", totalPowerToClaim,"\n");
+		require(totalPowerToClaim > 0, "No tokens to claim");												// Controllo che ci siano token da claimare
+
+		// Calcolo il numero dei token da distribuire all'utente, con la seguente proporzione:
+		// totalTokenToDistribute : TotalPower = userTokenToClaim : totalPowerToClaim
+		uint256 Moltiplicatore = 10**18;																	// moltiplicatore per evitare problemi di arrotondamento
+
+		// La formula è la seguente:
+		// userTokenToClaim = totalTokenToDistribute * (totalPowerToClaim / TotalPower)
+		// ma questa formula ha un problema di arrotondamento, per questo motivo moltiplico e poi divido per 10^18 (Moltiplicatore)
+		uint256 userTokenToClaim = totalTokenToDistribute.mul(Moltiplicatore).mul(totalPowerToClaim).div(TotalPower).div(Moltiplicatore); 
+		//console.log("totalTokenToDistribute before Claim: ", totalTokenToDistribute);
+
+		require(userTokenToClaim > 0, "No tokens to claim");                                 // Controllo che ci siano token da claimare (check)
+
+		// Riumovo dal totale i token da distribuire
+		totalTokenToDistribute = totalTokenToDistribute.sub(userTokenToClaim);                 // (effect)
+
+		// Riumovo dal il power dell'utente dal totale dei power
+		TotalPower = TotalPower.sub(totalPowerToClaim);                                        // (effect)
+
+		// Trasferisco i token all'utente
+		token.transfer(msg.sender, userTokenToClaim);                                        // (interaction)
+
+		console.log("userTokenToClaim: ", userTokenToClaim, " totalTokenToDistribute after claim: ", totalTokenToDistribute);
+
+		emit tokenClaimed(msg.sender, userTokenToClaim, totalTokenToDistribute);
+	}
+
+
 
 	// GETTERs & SETTERs
 	function getMyOrders() public view returns (uint256[] memory)
@@ -221,5 +281,19 @@ contract Launchpool is Ownable, ReentrancyGuard {
 			_end
 		);
 	}
+
+	// CHECKERs
+	function isStarted() public view returns (bool) {
+		return block.timestamp >= startLP;
+	}
+
+	function isEnded() public view returns (bool) {
+		return block.timestamp > endLP;
+	}
+
+	function isClaimed(uint256 _orderID) public view returns (bool) {
+		return orders[_orderID].isClaimed;
+	}
+
 
 }
