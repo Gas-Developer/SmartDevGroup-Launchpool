5e987eaa50108e0e2ca37c9ee93cc3a8e5d7210e
Gas-Developer <gascriptocoin@gmail.com>
2023-08-07 12:43:29 +0200

    minor clean

--- lpool-fe/src/components/LandingPage.tsx
+++ lpool-fe/src/components/LandingPage.tsx
@@ -1,15 +1,11 @@
 'use client'
 
 import { UserTypeButton } from "../components/buttons/userType-button";
-import { Connect } from "../components/Connect";
-import { useContractRead } from "wagmi";
-import { NetworkSwitcher } from "../components/NetworkSwitcher";
-import { wagmiContractConfig } from "../abi/launchpool-abi";
-import { useState } from "react";
+//import { useState } from "react";
 
 export default function LandingPage() {
 
-	const [isStarted, setIsStarted] = useState(false);
+	//const [isStarted, setIsStarted] = useState(false);
 
 	return (
 		<div>
