b022284ed09b2a5102affb5bc061ee3db6d8661f
RiccardoMolinari95 <riccardo95molinari@gmail.com>
2023-08-14 16:24:26 +0200

    Fixed areaStatsprops state

--- lpool-fe/src/components/area/AreasContainer.tsx
+++ lpool-fe/src/components/area/AreasContainer.tsx
@@ -4,7 +4,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { ContractData } from "../interfaces/ContractData";
 import { AreaControls } from "./AreaControls";
 import { AreaStats } from "./AreaStats";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 const logger = require("pino")();
 
 export function AreasContainer(props: ContractData) {
@@ -12,26 +12,38 @@ export function AreasContainer(props: ContractData) {
 	const cData: ContractData = {...props};
 	
 	const pathname = usePathname();
-
 	const userType = pathname.includes('investor') ? "investor" : "creator";
 
+	let filled = useRef(false);
+
 	// GESTISCO I DATI DELLE STATS CON GLI STATI
-	const [AreaStatsProps, setAreaStatsProps] = useState({
+	const [areaStatsProps, setAreaStatsProps] = useState({
 		userType: userType,
 		cData: cData,
 	});
 
-	logger.info(AreaStatsProps);	
+	useEffect(() => {
+		
+		const newAreaStatsProps = { ...areaStatsProps };
+        newAreaStatsProps.cData = cData;
 
-	const AreaControlsProps = {
-		setTokenData: setTokenData,
-		cData: cData,
-	};
+		if (Object.keys(newAreaStatsProps.cData).length !== 0 && !filled.current) {
+            setAreaStatsProps(newAreaStatsProps);
+            filled.current = true;
+        }
+
+	}, [cData]);
+
+	const areaControlsProps = {
+        setTokenData: setTokenData,
+        cData: cData,
+        userType: userType
+    };
 
 	// FUNCTIONS
 	function setTokenData(data: any, newTokenAddress: string) {
 
-		const newAreaStatsProps = {...AreaStatsProps};
+		const newAreaStatsProps = {...areaStatsProps};
 		newAreaStatsProps.cData.name = data?.[0].result;
 		newAreaStatsProps.cData.symbol = data?.[1].result;
 		newAreaStatsProps.cData.token = newTokenAddress;
@@ -41,18 +53,18 @@ export function AreasContainer(props: ContractData) {
 	}
 
 	return (
-		<>
-		<div id="dashboardTitleContainer">
-			<h1 id="containerAreasTitle">
-				{userType === "investor"
-					? "Investor Dashboard"
-					: "Creator Dashboard"}{" "}
-				</h1>
-				</div>
-			<AreaStats {...AreaStatsProps} />
-			<AreaControls {...AreaControlsProps} />
-		</>
-	);
+        <>
+            <div id="dashboardTitleContainer">
+                <h1 id="containerAreasTitle">
+                    {userType === "investor"
+                        ? "Investor Dashboard"
+                        : "Creator Dashboard"}
+                </h1>
+            </div>
+            <AreaStats {...areaStatsProps} />
+            <AreaControls {...areaControlsProps} />
+        </>
+    );
 }
 
 
