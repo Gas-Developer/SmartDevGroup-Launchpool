37e910a847d217ccfbd806b71ed86a9bf83cab2e
RiccardoMolinari95 <riccardo95molinari@gmail.com>
2023-08-16 16:56:22 +0200

    Datetimepicker for launchpool creation

--- lpool-fe/src/components/CreateLaunchpool.tsx
+++ lpool-fe/src/components/CreateLaunchpool.tsx
@@ -1,13 +1,12 @@
-import { SetStateAction, useEffect, useState } from "react";
+import { useState } from "react";
 import { ControlButton } from "./buttons/ControlButton";
-import { Textfield } from "./input/Textfield";
 import { ControlButtonData } from "./interfaces/ControlButtonData";
 import { LinkToken } from './LinkToken';
 import { useContractWrite, useWaitForTransaction } from "wagmi";
-import { wagmiContractConfig } from "../abi/launchpool-abi";
-import { write } from "fs";
 import { FactoryContractConfig } from "../abi/factory-abi";
 import { BaseError, stringify } from "viem";
+import DateTimePicker from "./dateTimePicker";
+const logger = require("pino")();
 
 const defaultTF = {
 	id: "",
@@ -54,21 +53,30 @@ export function CreateLaunchpool(props: any) {
 	function createLaunchpool() {
 
 		console.log("createLaunchpool");
+		logger.info("startLPValue", startLPValue);
+		logger.info("createLauendLPValuenchpool", endLPValue);
 
-		if(startLPValue != undefined && startLPValue > 0 && endLPValue != undefined && endLPValue > 0 && tokenAddress != undefined && tokenAddress.startsWith('0x')) {
+		if (startLPValue != undefined && startLPValue > 0 && endLPValue != undefined && endLPValue > 0 && tokenAddress != undefined && tokenAddress.startsWith('0x')) {
+			
+			const startLPValueInSeconds = startLPValue / 1000;
+			const endLPValueInSeconds = endLPValue / 1000;
 
+			logger.info("startLPValueInSeconds", startLPValueInSeconds);
+            logger.info("endLPValueInSeconds", endLPValueInSeconds);
+
+			logger.info("createLaunchpool",setEndLPValue);
 			write({
-				args: [
-					FactoryContractConfig.templateAddress, 
-					tokenAddress as `0x${string}`, 
-					BigInt(startLPValue), 
-					BigInt(endLPValue)
-				],
-			})
+                args: [
+                    FactoryContractConfig.templateAddress,
+                    tokenAddress as `0x${string}`,
+                    BigInt(startLPValueInSeconds),
+                    BigInt(endLPValueInSeconds),
+                ],
+            });
 
 		} else {
-			console.log("ERROR: createLaunchpool");
-			console.log("Some input datas are missing or wrong");
+			logger.info("ERROR: createLaunchpool");
+			logger.info("Some input datas are missing or wrong");
 		}
 
 	}
@@ -102,46 +110,49 @@ export function CreateLaunchpool(props: any) {
 	// TOKEN ADDRESS
 
 	return (
-		<>
-			Create Launchpool
-			<ul className="list-group controls-list-group">
-				<li className="list-group-item  controls-list-group-item">
-					<LinkToken {...linkTokenData} />
-				</li>
-				<li className="list-group-item controls-list-group-item">
-					<Textfield {...defaultTF}
-							id="start_lp" 
-							name="start_lp" 
-							placeholder="Start: --/--/---- --:--:--" 
-							value={startLPValue} 
-							onChange={(e: { target: { value: SetStateAction<undefined>; }; }) => setStartLPValue(e.target.value)} 
-					/>
-				</li>
-				<li className="list-group-item controls-list-group-item">
-					<Textfield {...defaultTF}
-							id="end_lp" 
-							name="end_lp" 
-							placeholder="End: --/--/---- --:--:--" 
-							value={endLPValue} 
-							onChange={(e: { target: { value: SetStateAction<undefined>; }; }) => setEndLPValue(e.target.value)} 
-					/>
-				</li>
-				<li className="list-group-item controls-list-group-item">
-					{ 
-						isPending ?
-							<div>Transaction pending...</div> :
-							<ControlButton {...create_launchpool}/>
-					}
+        <>
+            <h4 style={{ color:'white'}}>Create Launchpool</h4>
+            <ul className="list-group controls-list-group">
+                <li className="list-group-item  controls-list-group-item">
+                    <LinkToken {...linkTokenData} />
+                </li>
+                <li className="list-group-item controls-list-group-item">
+                    <DateTimePicker
+                        id="start_lp"
+                        placeholder="Enter launchpool start time"
+                        calendarInputClass="form-control controls-textfield"
+                        setStartLPValue={setStartLPValue}
+                        calendarInputSize={40}
+                    />
+                </li>
+                <li className="list-group-item controls-list-group-item">
+                    <DateTimePicker
+                        id="end_lp"
+                        placeholder="Enter launchpool end time"
+                        calendarInputClass="form-control controls-textfield"
+                        setEndLPValue={setEndLPValue}
+                        calendarInputSize={40}
+                    />
+                </li>
+                <li className="list-group-item controls-list-group-item">
+                    {isPending ? (
+                        <div>Transaction pending...</div>
+                    ) : (
+                        <ControlButton {...create_launchpool} />
+                    )}
 
-					{isSuccess && (
-						<div>
-							<a href={`https://mumbai.polygonscan.com/address/${data?.hash}`}>Transaction</a>
-						</div>
-					)}
-					{isError && <div>{(error as BaseError)?.shortMessage}</div>}
-
-				</li>
-			</ul>
-		</>
-	);
+                    {isSuccess && (
+                        <div>
+                            <a
+                                href={`https://mumbai.polygonscan.com/address/${data?.hash}`}
+                            >
+                                Transaction
+                            </a>
+                        </div>
+                    )}
+                    {isError && <div>{(error as BaseError)?.shortMessage}</div>}
+                </li>
+            </ul>
+        </>
+    );
 }
