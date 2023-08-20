b83d253f5621ccc7559c6884bdd3f7b02205637e
Gas-Developer <gascriptocoin@gmail.com>
2023-08-20 11:00:37 +0200

    UserType Button Graphic support

--- lpool-fe/src/components/buttons/userType-button.tsx
+++ lpool-fe/src/components/buttons/userType-button.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import Link from "next/link";
+import ExagonalBTNInvestor from "../../assets/images/ExagonalBTNInvestor.png";
+import ExagonalBTNCreator from "../../assets/images/ExagonalBTNCreator.png";
+import Image from 'next/image'
 
 export function UserTypeButton(props: any) {
 
-  const name = props.name;
+	const name = props.name;
 
-  const marginRight = name === "Investor" ? "margin-right" : "";
-  let href = name === "Investor" ? "/dashboard/investor" : "/dashboard/creator";
+	const marginRight = name === "Investor" ? "margin-right" : "";
+	let href = name === "Investor" ? "/dashboard/investor" : "/dashboard/creator";
+	let image = name === "Investor" ? ExagonalBTNInvestor : ExagonalBTNCreator;
 
-  return (
-    <Link href={href} className="userTypeButton marginRight">{name}</Link>
-  );
+	return (
+		<>
+			<a href={href}>
+				<Image
+					src={image}
+					alt="Area Button"
+					width={450}
+					height={400}
+				/>
+			</a>
+		</>
+
+	);
 }
