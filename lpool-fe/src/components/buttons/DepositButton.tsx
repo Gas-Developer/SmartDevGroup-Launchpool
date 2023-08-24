dae51278e4fd37c3ab9b4489f35a438ad951e8aa
Gas-Developer <gascriptocoin@gmail.com>
2023-08-24 16:20:50 +0200

    DepositButton 1.5

--- lpool-fe/src/components/buttons/DepositButton.tsx
+++ lpool-fe/src/components/buttons/DepositButton.tsx
@@ -1,23 +1,58 @@
-import { useContractRead } from "wagmi";
+import { useContractRead, useContractWrite, useWaitForTransaction } from "wagmi";
 import { ControlButtonData } from "../interfaces/ControlButtonData";
 import { FactoryContractConfig } from "../../abi/factory-abi";
 import { useState } from "react";
+import { LaunchpoolContractConfig } from "../../abi/launchpool-abi";
 
-export function DepositButton(props: ControlButtonData) {
+export function DepositButton(props: any) {
 
-	const className = "btn btn-primary controlButton "+props.className;
+	// DEPOSIT
+	const { write, data, error, isLoading, isError } = useContractWrite({
+		...LaunchpoolContractConfig,
+		address: props.launchpoolAddress,
+		functionName: 'depositTokenToDistribute',
+
+	})
+
+	const {
+		data: receipt,
+		isLoading: isPending,
+		isSuccess,
+	} = useWaitForTransaction(
+		{ 
+			hash: data?.hash,
+			onSuccess(data) {
+				console.log('TX Success');
+				console.log('Deposito effettuato corretamente');
+				props.setIsDeposited(true);
+			},
+		}
+	)
+
+	function deposit() {
+		console.log("deposit");
+		console.log("TODO: Eseguire controlli di eccesso quantità e effettuare la conversione in Wei o Matic");
+		//TODO: Eseguire controlli di eccesso quantità e effettuare la conversione in Wei o Matic
+
+		// DEPOSIT
+		write({
+			args: [
+				BigInt(props.depositQty)
+			],
+		})
+	}
 
 	return (
 		<div>
 			<input 
 				type="button" 
-				name={props.name} 
-				id={props.name} 
-				className={className} 
-				value={props.text} 
-				onClick={props.onClick} 
-				title={props.tooltip} 
-				disabled={props.disabled}
+				name="deposit_button"
+				id="deposit_button"
+				className={props.className} 
+				value="Deposit" 
+				onClick={deposit} 
+				title="Deposit"
+				disabled={false}
 			/>
 		</div>
 	);
