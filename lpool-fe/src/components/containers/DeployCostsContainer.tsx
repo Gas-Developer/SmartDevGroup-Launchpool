08106ac15f707614104e97da573c92dd320429c8
Gas-Developer <gascriptocoin@gmail.com>
2023-08-27 15:27:56 +0200

    DeployCostContainer & LPCardPreviewContainer created

---
+++ lpool-fe/src/components/containers/DeployCostsContainer.tsx
@@ -0,0 +1,18 @@
+import { DeployForm } from "../forms/DeployForm";
+import DefaultContainer from "./DefaultContainer";
+import TrasparentContainer from "./TrasparentContainer";
+
+
+export function DeployCostsContainer() {
+
+	return (
+		<>
+			<TrasparentContainer className="" >
+				<DefaultContainer className="">
+					DeployCostsContainer
+				</DefaultContainer>
+			</TrasparentContainer>
+		</>
+	);
+
+}
---
+++ lpool-fe/src/components/containers/LPCardPreviewContainer.tsx
@@ -0,0 +1,18 @@
+import { DeployForm } from "../forms/DeployForm";
+import DefaultContainer from "./DefaultContainer";
+import TrasparentContainer from "./TrasparentContainer";
+
+
+export function LPCardPreviewContainer() {
+
+	return (
+		<>
+			<TrasparentContainer className="" >
+				<DefaultContainer className="">
+				LPCardPreviewContainer
+				</DefaultContainer>
+			</TrasparentContainer>
+		</>
+	);
+
+}
