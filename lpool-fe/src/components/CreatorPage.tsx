55e1ff8973791b7a80ba61e6bca3495aeb28562a
Gas-Developer <gascriptocoin@gmail.com>
2023-08-27 15:30:12 +0200

    CreatorPage update

--- lpool-fe/src/components/CreatorPage.tsx
+++ lpool-fe/src/components/CreatorPage.tsx
@@ -2,6 +2,10 @@
 
 import { ContractData } from "./interfaces/ContractData";
 import { AreasContainer } from "./area/AreasContainer";
+import { LPCard } from "./cards/LPCard";
+import { DeployFormContainer } from "./containers/DeployFormContainer";
+import { LPCardPreviewContainer } from "./containers/LPCardPreviewContainer";
+import { DeployCostsContainer } from "./containers/DeployCostsContainer";
 
 const logger = require("pino")();
 
@@ -15,9 +19,44 @@ export function CreatorPage(props: any) {
 	if(contractData.cid === undefined && props.cid !== undefined)
 		contractData.cid = props.cid;
 
+	// TODO: Verify that this is the correct way to do this
+	// contractData.launchpoolAddress = contractData.launchpoolAddress ? contractData.launchpoolAddress : props.launchpoolAddress;
+	// contractData.cid = contractData.cid ? contractData.cid : props.cid;
+
 	return (
 		<>
-			<AreasContainer {...contractData} />
+			{/* <div className="grid grid-cols-4 gap-4">
+				<div className="col-span-2">
+					<LPCardPreviewContainer />
+				</div>
+				<div className="col-span-2">
+					<DeployFormContainer />
+				</div>
+				<div className="col-span-2">
+					<DeployCostsContainer />
+				</div>
+			</div> */}
+
+
+			<div className="grid grid-cols-2 gap-4">
+				<div className="col-span-1">
+					<div className="grid grid-rows-3 grid-flow-col gap-4 ">
+						<div className="row-span-2 ">
+							{/* <LPCard {...contractData} /> */}
+							<LPCardPreviewContainer />
+						</div>
+						<div className="row-span-1 ">
+							{/* <DeployCosts {...contractData} /> */}
+							<DeployCostsContainer />
+						</div>
+					</div>
+				</div>
+				<div className="col-span-1">
+					<DeployFormContainer />
+				</div>
+
+			</div>
+			{/* <AreasContainer {...contractData} /> */}
 		</>
 	);
 }
