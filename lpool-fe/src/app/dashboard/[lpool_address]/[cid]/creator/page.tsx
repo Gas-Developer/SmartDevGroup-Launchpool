f879d4461b574bdc88565e2780130c16eca84184
Gas-Developer <gascriptocoin@gmail.com>
2023-09-15 06:53:39 +0200

    CreatorLPCretedPAge - BaseLayout integration

--- lpool-fe/src/app/dashboard/[lpool_address]/[cid]/creator/page.tsx
+++ lpool-fe/src/app/dashboard/[lpool_address]/[cid]/creator/page.tsx
@@ -1,8 +1,10 @@
 import { CreatorLPCreatedPage } from "../../../../../components/CreatorLPCreatedPage";
+import { BaseLayout } from "../../../../../components/layout/BaseLayout";
 
 export default function Page({ params }: { params: { lpool_address: string, cid: string } }) {
 	return (
 		<>
+			<BaseLayout />
 			<CreatorLPCreatedPage launchpoolAddress={params.lpool_address} cid={params.cid}/>
 		</>
 	);
