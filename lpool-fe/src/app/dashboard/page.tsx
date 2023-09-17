636d914bdc013cecdd68222353964fb7dea72c8b
RiccardoMolinari95 <riccardo95molinari@gmail.com>
2023-09-17 10:13:32 +0200

    Add Page Container, commit for CORS problem

--- lpool-fe/src/app/dashboard/page.tsx
+++ lpool-fe/src/app/dashboard/page.tsx
@@ -1,12 +1,12 @@
 import DashboardMainContainer from "../../components/containers/DashboardMainContainer";
 import { BaseLayout } from "../../components/layout/BaseLayout";
 import "../../assets/styles/dashboard.css";
+import PageContainer from "../../components/containers/PageContainer";
 
 export default function Page() {
     return (
         <>
-            <BaseLayout />
-            <DashboardMainContainer />
+            <PageContainer />
         </>
     );
 }
